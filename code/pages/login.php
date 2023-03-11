15df3ce49d8db078849e7123048225fa09b55c87
jorubka <127230527+jorubka@users.noreply.github.com>
2023-03-11 19:28:27 +0000

    Updated Login and Register pages

--- code/pages/login.php
+++ code/pages/login.php
@@ -21,62 +21,27 @@ session_start();
 </head>
 
 <body>
-    
-    
-        <h1 align="center">Welcome to the Impact Evidence Platform. Please Log in to continue</h1>
-        <br />
-        <!-- Login form -->
-        
-        <!---->
-        <div  class = "content" align = "center">
-        <form class = "input" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                <fieldset style="width:350px" align="center">
-                    <legend align="left"><b>Log in</b></legend>
-                    <div>
-                        <label for="input_email">email:</label>
-                        <input type="text" name="email" size="24" /><br />
-                    </div>
-                    <br />
-                    <div>
-                        <label for="password">Password:</label>
-                        <input type="password" name="password" value="" size="24" /><br />
-                    </div>
-                </fieldset>
-                <fieldset style="width:350px" align="center"><input value="Submit" type="submit" class="button"/> &nbsp;
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input value="Reset" type="reset" class="button"/>
-                </fieldset>
-            </form>
-
-            <!--Run conductLogin.php file when form is submitted otherwise prompt user to enter details-->
-            
-               <!--- <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        require '../php/conductLogin.php';
-                    } else {
-                        echo "Input details to log on";
-                    }
-                    ?>; 
-                
-               
-    <!--        
-        </div>
-    -->
-
-
-    <!-- Login form -->
+  
 <section class="container2">
-
-<form action="" method="post">
-   <h3>log in</h3>
-   <input type="email" name="email" class="box" placeholder="Your email" required>
-   <input type="password" name="pass" class="box" placeholder="Your password" required>
-   <input type="submit" class="btn" name="submit" value="log in">
-   
-</form>
-
-    
+       
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+               
+        <h3>Log in</h3>
+                 
+        <input type="text" name="email" class="box" placeholder="Your email...">
+        <input type="password" name="password" class="box" placeholder="Your password...">        
+        <input value="Submit" type="submit" class="btn" name="Submit"> 
+        <div><br><a href="../pages/register.php">Create a new account</a></br></div>
+                
+    </form>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            require '../php/conductLogin.php';
+        } 
+    ?>
 
 </section>
+               
 </body>
 
 </html>
--- code/pages/register.php
+++ code/pages/register.php
@@ -22,64 +22,28 @@ session_start();
 </head>
 
 <body>
-    <!-- ---------------- -->
-    <div class="content">
-        <!-- Content -->
-        <h1 align="center">Please enter new account details to sign up!</h1>
-        <br />
-    
+
         <!-- Registration form form -->
-        
-        <div align="center">
+    
+        <section class="container2">
             <form class="input" name="register" onsubmit="return validateRegisterForm()" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <fieldset style="width:350px" align="right">
-                    <legend align="left"><b>Registration Form</b></legend>
-                    <div>
-                        <label for="email">Email:</label>
-                        <input type="text" name="email" size="24" /><br />
-                    </div>
-                    <br />
-                    <div>
-                        <label for="password">Password:</label>
-                        <input type="password" name="password" value="" size="24" /><br />
-                    </div>
-                    <br />
-                    <div>
-                        <label for="password">Re-enter Password:</label>
-                        <input type="password" name="repassword" value="" size="24" /><br />
-                    </div>
-                </fieldset>
-                <fieldset style="width:350px" align="center">
-                    <input value="Submit" type="submit" /> &nbsp;
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input value="Reset" type="reset" />
-                </fieldset>
+
+                    <h3>Register</h3>
+                    <input type="text" name="email" class="box" placeholder="Your email...">
+                    <input type="password" name="password" class="box" placeholder="Your password...">
+                    <input type="password" name="repassword" class="box" placeholder="Repeat your password...">
+                    <input value="Create account" class="btn" name="submit" type="submit">
+                    <div><br><a href="../pages/login.php">Already have an account? Sign in</a></br></div>
+
             </form>
-        </div>
+  
         
         <!--Run registering.php file when form is submitted-->
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             require '../php/registering.php';
-        } else {
-            echo "Input details to register";
-        }
-        ?>;
-    </div>
-
-
-
-    <!-- Register form -->
-<section class="container2">
-
-<form action="" method="post">
-   <h3>Register</h3>
-   <input type="text" name="name" class="box" placeholder="Your Email" required>
-   <input type="password" name="pass" class="box" placeholder="Your password" required>
-   <input type="password" name="cpass" class="box" placeholder="confirm your password" required>
-   <input type="submit" class="btn" name="submit" value="Create account">
-</form>
-
-</section>
+        } 
+        ?>
 
 </body>
 
