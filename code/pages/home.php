bdacc869c2280493dcdad22e0e04c1f4bb370080
darkbluepe1 <127230896+darkbluepe1@users.noreply.github.com>
2023-03-13 21:08:43 +0000

    Changes to home and header
    Made login/logout buttons white for readability
    
    Made UOB logo transparent for aestetics.
    
    Made catagories on homepage 2x2 instead of 3x1.

--- code/pages/home.php
+++ code/pages/home.php
@@ -19,30 +19,72 @@
          </h1>
       </div>
       <br><br><br>
+      
+
       <div class ="container">
          <div class="row gy-5">
+
+
+         <?php
+            if (isset($_SESSION["logon"])) {
+               if ($_SESSION["admin"] == true) {//if admin show admin links
+                  echo("
+
+            <div class='col-lg-4'>
+               <div class='p-3 border bg-light'>
+                  <div class='res'>
+                     <h2> Dashboard </h2>
+                  </div>
+                  <img src='https://i.imgur.com/iGPyZYp.png'>
+                  <div class='clickhere'>
+                     <a href='#Dashboard'>
+                        <h3>Click Here</h3>
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <div class='col-lg-4'>
+               <div class='p-3 border bg-light'>
+                  <div class='imp'>
+                     <h2> Master Spreadsheet </h2>
+                  </div>
+                  <img src='https://i.imgur.com/665Hakv.png'>
+                  <div class='clickhere'>
+                     <a href='#Spreadsheet'>
+                        <h3>Click Here</h3>
+                     </a>
+                  </div>
+               </div>
+            </div>
+
+            ");
+         }
+      }
+   ?>
+
+
             <div class="col-lg-6">
                <div class="p-3 border bg-light">
-                  <div class="res">
+                  <div class="col">
                      <h2> Research Project Records </h2>
                   </div>
-                  <img src="https://i.imgur.com/y42BKUN.png"></a>
+                  <img src="https://i.imgur.com/jQKhmPH.png">
                   <div class="clickhere">
-                     <a href="#research">
-                        <h3><a href="resimpproject.php">Click Here</h3>
+                     <a href="resimpproject.php">
+                        <h3>Click Here</h3>
                      </a>
                   </div>
                </div>
             </div>
             <div class="col-lg-6">
                <div class="p-3 border bg-light">
-                  <div class="imp">
+                  <div class="Reviewers">
                      <h2> Impact Records </h2>
                   </div>
                   <img src="https://i.imgur.com/uosGz2q.png">
                   <div class="clickhere">
-                     <a href="#impact">
-                        <h3><a href="impactproject.php">Click Here</h3>
+                     <a href="impactproject.php">
+                        <h3>Click Here</h3>
                      </a>
                   </div>
                </div>
@@ -54,7 +96,9 @@
                   </div>
                   <img src="https://i.imgur.com/XN4tpER.png">
                   <div class="clickhere">
+
                      <a href="collab.php">
+
                         <h3>Click Here</h3>
                      </a>
                   </div>
@@ -65,9 +109,11 @@
                   <div class="Reviewers">
                      <h2> Reviewers </h2>
                   </div>
-                  <img src="https://i.imgur.com/jQKhmPH.png">
+                  <img src="https://i.imgur.com/USd6B2v.png">
                   <div class="clickhere">
+
                      <a href="rev.php">
+
                         <h3>Click Here</h3>
                      </a>
                   </div>
@@ -75,5 +121,10 @@
             </div>
          </div>
       </div>
+         
+                    
+                     
+
+            
    </body>
 </html>
