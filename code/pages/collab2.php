e15fd6b6e097ae68f705e479bfbe41f852500ddb
Sham199 <104640403+Sham199@users.noreply.github.com>
2023-03-14 12:49:37 +0000

    Update collab2.php

--- code/pages/collab2.php
+++ code/pages/collab2.php
@@ -22,122 +22,91 @@ session_start();
 
 <body>
 
-    <div class="container4">
-        <!-- back button -->
-        <a class="back-btn" href="home.php" > <img src="../img/back.png" width="50" height="50"></a>
-        <h2>Collaborator name</h2>
+   <div class="container4">
+      <!-- back button -->
+      <a class="back-btn" href="home.php" > <img src="../img/back.png" width="50" height="50"></a>
+         <h2>Collaborator name</h2>
       
         <!-- filter by option -->
-        <select class="filter-select">
-			<option value="all">Filter By</option>
-			<option value="alphabet">Alphabet</option>
-			<option value="department">Department</option>
-		</select>
-    </div>
+         <select class="filter-select">
+		      <option value="all">Filter By</option>
+			   <option value="alphabet">Alphabet</option>
+			   <option value="department">Department</option>
+		   </select>
+   </div>
+   <br><br>
 
+   <div class ="container">
+      <div class="row gy-5">
 
-    <div class ="container">
-         <div class="row gy-5">
+         <div class="col-lg-4">
+            <div class="p-3 border bg-light">
+               <div class="res">
+                  <h2> Edit Impact records</h2>
+               </div>
+               <img src="https://i.imgur.com/iGPyZYp.png">
 
-
-         <?php
-            if (isset($_SESSION["logon"])) {
-               if ($_SESSION["admin"] == true) {//if admin show admin links
-                  echo("
-
-            <div class='col-lg-4'>
-               <div class='p-3 border bg-light'>
-                  <div class='res'>
-                     <h2> Dashboard </h2>
-                  </div>
-                  <img src='https://i.imgur.com/iGPyZYp.png'>
-                  <div class='clickhere'>
-                     <a href='#Dashboard'>
-                        <h3>Click Here</h3>
-                     </a>
-                  </div>
+               <div class="clickhere">
+                  <a href="#Edit">
+                     <h3>Click Here</h3>
+                  </a>
                </div>
             </div>
-            <div class='col-lg-4'>
-               <div class='p-3 border bg-light'>
-                  <div class='imp'>
-                     <h2> Master Spreadsheet </h2>
-                  </div>
-                  <img src='https://i.imgur.com/665Hakv.png'>
-                  <div class='clickhere'>
-                     <a href='#Spreadsheet'>
-                        <h3>Click Here</h3>
-                     </a>
-                  </div>
+         </div>
+
+         <div class="col-lg-4">
+            <div class="p-3 border bg-light">
+               <div class="col">
+                  <h2> Upload Impact records </h2>
+               </div>
+               <img src="https://i.imgur.com/jQKhmPH.png">
+               
+               <div class="clickhere">
+                  <a href="#Upload">
+                     <h3>Click Here</h3>
+                  </a>
                </div>
             </div>
+         </div>
+   </div>
 
-            ");
-         }
-      }
-   ?>
+   <br><br>    
+   <div class ="container">
+      <div class="row gy-5">
 
-
-            <div class="col-lg-4">
-               <div class="p-3 border bg-light">
-                  <div class="col">
-                     <h2> Research Project Records </h2>
-                  </div>
-                  <img src="https://i.imgur.com/jQKhmPH.png">
-                  <div class="clickhere">
-                     <a href="resimpproject.php">
-                        <h3>Click Here</h3>
-                     </a>
-                  </div>
+         <div class="col-lg-4">
+            <div class="p-3 border bg-light">
+               <div class="col">
+                  <h2> Edit research project records </h2>
+               </div>
+               <img src="https://i.imgur.com/iGPyZYp.png">
+               
+               <div class="clickhere">
+                  <a href="#Edit">
+                     <h3>Click Here</h3>
+                  </a>
                </div>
             </div>
-            <div class="col-lg-4">
-               <div class="p-3 border bg-light">
-                  <div class="Reviewers">
-                     <h2> Impact Records </h2>
-                  </div>
-                  <img src="https://i.imgur.com/uosGz2q.png">
-                  <div class="clickhere">
-                     <a href="impactproject.php">
-                        <h3>Click Here</h3>
-                     </a>
-                  </div>
+         </div>
+
+         <div class="col-lg-4">
+            <div class="p-3 border bg-light">
+               <div class="col">
+                  <h2> Upload research project records </h2>
                </div>
-            </div>
-            <div class="col-lg-4">
-               <div class="p-3 border bg-light">
-                  <div class="col">
-                     <h2> Collaborators </h2>
-                  </div>
-                  <img src="https://i.imgur.com/XN4tpER.png">
-                  <div class="clickhere">
+               <img src="https://i.imgur.com/jQKhmPH.png">
 
-                     <a href="collab.php">
-
-                        <h3>Click Here</h3>
-                     </a>
-                  </div>
-               </div>
-            </div>
-            <div class="col-lg-4">
-               <div class="p-3 border bg-light">
-                  <div class="Reviewers">
-                     <h2> Reviewers </h2>
-                  </div>
-                  <img src="https://i.imgur.com/USd6B2v.png">
-                  <div class="clickhere">
-
-                     <a href="rev.php">
-
-                        <h3>Click Here</h3>
-                     </a>
-                  </div>
+               <div class="clickhere">
+                  <a href="#Upload">
+                     <h3>Click Here</h3>
+                  </a>
                </div>
             </div>
          </div>
       </div>
+   </div>
 
-
+   
 
 </body>
 
