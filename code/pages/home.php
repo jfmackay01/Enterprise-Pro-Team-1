f280983bc4f1da55243f61c50df1f47097cbb3ff
jfmackay01 <127228811+jfmackay01@users.noreply.github.com>
2023-03-14 16:21:58 +0000

    Update home.php

--- code/pages/home.php
+++ code/pages/home.php
@@ -29,7 +29,7 @@
             if (isset($_SESSION["logon"])) {
                if ($_SESSION["admin"] == true) {//if admin show admin links
                   echo("
-            <div class='col-lg-4'>
+            <div class='col-lg-6'>
                <div class='p-3 border bg-light'>
                   <div class='res'>
                      <h2> Dashboard </h2>
@@ -42,7 +42,7 @@
                   </div>
                </div>
             </div>
-            <div class='col-lg-4'>
+            <div class='col-lg-6'>
                <div class='p-3 border bg-light'>
                   <div class='imp'>
                      <h2> Master Spreadsheet </h2>
@@ -73,7 +73,7 @@
                   </div>
                </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                <div class="p-3 border bg-light">
                   <div class="Reviewers">
                      <h2> Impact Records </h2>
@@ -86,7 +86,7 @@
                   </div>
                </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                <div class="p-3 border bg-light">
                   <div class="col">
                      <h2> Collaborators </h2>
@@ -101,7 +101,7 @@
                   </div>
                </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                <div class="p-3 border bg-light">
                   <div class="Reviewers">
                      <h2> Reviewers </h2>
