4a95ee37af011dcd2cf4600261d1c8b0d6b4b016
Sham199 <104640403+Sham199@users.noreply.github.com>
2023-05-03 18:31:55 +0100

    Update viewAllImpact.php

--- Software/pages/viewAllImpact.php
+++ Software/pages/viewAllImpact.php
@@ -5,15 +5,12 @@ session_start();
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="../css/standard.css">
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>View all impact</title>
-
+    
     <?php
     require 'header.php';
     ?>
+    
+    <title>View all impact</title>
 
 </head>
 
