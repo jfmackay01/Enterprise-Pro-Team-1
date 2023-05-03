f9c96766d45a5c1ad5b9c0fee0740d97859108db
Sham199 <104640403+Sham199@users.noreply.github.com>
2023-05-03 18:24:20 +0100

    Update projects.php

--- Software/pages/projects.php
+++ Software/pages/projects.php
@@ -5,10 +5,7 @@ session_start();
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="../css/standard.css">
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title>projects</title>
 
     <?php
