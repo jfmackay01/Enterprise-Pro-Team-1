ae6f9eb1c98a85bb786689bf9578360973641789
Sham199 <104640403+Sham199@users.noreply.github.com>
2023-05-03 18:31:29 +0100

    Update users.php

--- Software/pages/users.php
+++ Software/pages/users.php
@@ -6,16 +6,13 @@ session_start();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Users</title>
+   <?php 
+    require 'header.php'; 
+   ?>
+    
+   <title>Users</title>
 
-    <?php require 'header.php'; ?>
-
-    <!-- CSS styles -->
-    <link rel="stylesheet" href="../css/standard.css">
 </head>
 
 <body>
